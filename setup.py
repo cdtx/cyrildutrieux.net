60a1ef84912d299916e901db159d128c41b52819
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2016-07-03 23:36:41 +0200

    Added setuptools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cyrildutrieux',
+    version='0.0.1',
+    description='My website',
+    author='cdtx',
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=['cdtx.cyrildutrieux'],
+    install_requires = []
+)
+
+
