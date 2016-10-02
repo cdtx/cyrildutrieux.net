8c6bf0c0753cf943d39203e45bb5ebd1a879d763
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2016-10-03 00:15:23 +0200

    Added content Trial to move the whole content in a side file

---
+++ cdtx/cyrildutrieux/static/js/content.js
@@ -0,0 +1,15 @@
+var content = {
+    'name': 'Cyril DUTRIEUX',
+    'function': {
+        'fr': 'Développeur logiciel indépendant',
+        'en': 'Freelance software developper',
+    },
+    'card': {
+        '0': {
+            'title': {
+                'fr': '',
+                'en': '',
+            }
+        }
+    }
+}
